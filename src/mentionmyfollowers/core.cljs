a7ea4296ee7814589321532b29131ab8b4f00fcf
gmihai <mihai.giurgeanu@gmail.com>
2015-03-08 20:56:44 +0000

    Using om and domina.

--- src/mentionmyfollowers/core.cljs
+++ src/mentionmyfollowers/core.cljs
@@ -1,3 +1,22 @@
-(ns mentionmyfollowers.core)
+(ns mentionmyfollowers.core
+    (:require[om.core :as om :include-macros true]
+             [om.dom :as dom :include-macros true]
+             [domina.css :as css]))
+
+(enable-console-print!)
 
 (.write js/document "<h1>Hello world</h1>")
+
+(defonce app-state (atom {:user-id nil}))
+
+(defn show-user-id [{:keys [user-id]} owner]
+    (reify om/IRender
+        (render [_]
+                (dom/p 
+                    nil
+                    (str "User-id is "(if user-id user-id "Not Available"))))))
+
+(om/root
+    show-user-id
+    app-state
+    {:target (css/sel "#app")})
